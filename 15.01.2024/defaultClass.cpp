853ae6ec86fc34708512ae256a5f1dcda7650981
Harley <92978645+BeidinaDaria@users.noreply.github.com>
2024-01-15 19:41:32 +0300

    Add files via upload

---
+++ 15.01.2024/defaultClass.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+class Student {
+private:
+    std::string name;
+    int age;
+public:
+    Student(std::string name, int age) {
+        this->age = age;
+        this->name = name;
+    }
+    void show() {
+        std::cout << "Name: " << name << " age: " << age << std::endl;
+    }
+};
+
+int main(){
+    Student st1("Paul",22);
+    st1.show();
+}
